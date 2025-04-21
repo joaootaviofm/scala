c24d9e8157b6b8c7f49fcfd552a5d79e48e04a02
joaootaviofm <joaootaviofm@gmail.com>
2025-04-21 15:18:28 -0300

    feat: added dropdown menu for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
+"use client";
 import { HiOutlineMenu } from "react-icons/hi";
+import { useState } from "react";
 
 export default function Navbar() {
   const navItems = ["Início", "Serviços", "Sobre", "Como funciona?"];
+
+  const [navOpen, setNavOpen] = useState(false);
 
   return (
     <nav className="backdrop-blur-2xl bg-[#181818]/20 border-b border-white/10 fixed z-10 w-full flex items-center justify-around py-14 font-regular text-white">
@@ -31,7 +35,28 @@ export default function Navbar() {
         </a>
       </div>
       <div className="flex md:hidden">
-        <HiOutlineMenu className="text-3xl"/>
+        <HiOutlineMenu
+          onClick={() => setNavOpen(!navOpen)}
+          className="text-3xl text-white cursor-pointer"
+        />
+        {navOpen && (
+          <ul className="bg-black border border-white/30 text-white rounded-2xl absolute w-[80%] top-[160px] left-1/2 -translate-x-1/2 p-4 z-50 shadow-white/40 shadow-lg">
+            {navItems.map((item, index) => (
+              <li
+                key={index}
+                className="py-3 text-center border-b border-white/10 last:border-0"
+              >
+                <a
+                  href="#"
+                  className="block hover:text-[#008fff] transition-colors duration-300"
+                  onClick={() => setNavOpen(false)}
+                >
+                  {item}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </nav>
   );
