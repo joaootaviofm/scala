fd25040cf8e3bba4c47d1b0a2b0f0bb2dd4e3077
joaootaviofm <joaootaviofm@gmail.com>
2025-04-21 15:41:22 -0300

    feaT: added motion to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import { motion } from "framer-motion";
 import { HiOutlineMenu } from "react-icons/hi";
 import { useState } from "react";
 
@@ -10,29 +12,43 @@ export default function Navbar() {
   return (
     <nav className="backdrop-blur-2xl bg-[#181818]/20 border-b border-white/10 fixed z-10 w-full flex items-center justify-around py-14 font-regular text-white">
       <div>
-        <h1 className="tracking-[2px] text-4xl font-bold hover:scale-105 duration-300 cursor-default">
+        <motion.h1
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5 }}
+          className="tracking-[2px] text-4xl font-bold hover:scale-105 duration-300 cursor-default"
+        >
           scala <span className="text-[#008fff]">ai</span>
-        </h1>
+        </motion.h1>
       </div>
       <ul className="md:flex gap-10 hidden">
         {navItems.map((item, index) => (
-          <li
+          <motion.li
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6, delay: 0.3 * index }}
             key={index}
             className="hover:scale-110 duration-300 border-b border-transparent hover:border-white py-[2px]"
           >
             <a href="#" className="tracking-[1px]">
               {item}
             </a>
-          </li>
+          </motion.li>
         ))}
       </ul>
       <div className="hover:scale-110 duration-300 md:flex hidden">
-        <a
+        <motion.a
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 1}}
           href="#"
           className="tracking-[1px] text-[18px] border font-light border-white hover:bg-white hover:text-[#181818] duration-300 px-5 py-4"
         >
           Contato
-        </a>
+        </motion.a>
       </div>
       <div className="flex md:hidden">
         <HiOutlineMenu
