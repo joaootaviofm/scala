2c7d977738a4a20560ca11f35012b6a5e060b296
joaootaviofm <joaootaviofm@gmail.com>
2025-04-21 15:47:56 -0300

    feat: added motion to services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 export default function Services() {
   const services = [
     {
@@ -34,15 +36,30 @@ export default function Services() {
 
   return (
     <section className="text-white px-5 py-16 flex flex-col justify-center items-center">
-      <h1 className="tracking-[2px] text-[60px] font-bold hover:scale-105 duration-300 cursor-default">
+      <motion.h1
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+        className="tracking-[2px] text-[60px] font-bold hover:scale-105 duration-300 cursor-default"
+      >
         Nossos <span className="text-[#008fff]">serviços</span>
-      </h1>
+      </motion.h1>
       <div className="mt-16 grid grid-cols-1 md:grid-cols-3 gap-10 max-w-[1200px]">
         {services.map((item, index) => (
-          <div key={index} className="px-5 py-5 bg-white/5 rounded-lg hover:scale-110 hover:shadow-2xl shadow-white/15 cursor-default duration-300 border border-white/20 backdrop-blur-md flex flex-col gap-6">
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5 , delay: 0.2*index }}
+            key={index}
+            className="px-5 py-5 bg-white/5 rounded-lg hover:scale-110 hover:shadow-2xl shadow-white/15 cursor-default duration-300 border border-white/20 backdrop-blur-md flex flex-col gap-6"
+          >
             <h1 className="text-center">{item.serviceName}</h1>
-            <p className="leading-relaxed text-center">{item.serviceDescription}</p>
-          </div>
+            <p className="leading-relaxed text-center">
+              {item.serviceDescription}
+            </p>
+          </motion.div>
         ))}
       </div>
     </section>
