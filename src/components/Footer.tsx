f068275e76026937c15faa4d2dea820fc4c42f59
joaootaviofm <joaootaviofm@gmail.com>
2025-04-21 19:42:08 -0300

    feat: cr footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <div className="italic text-center text-white bg-[#181818] ">
+            © 2024 Todos os direitos reservados por SCALA AI 
+        </div>
+    )
+}
