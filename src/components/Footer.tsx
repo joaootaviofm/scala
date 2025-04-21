19fe01f0fd75adbdbb8d222882391e08c01c59a2
joaootaviofm <joaootaviofm@gmail.com>
2025-04-21 19:46:04 -0300

    fixing year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return (
         <div className="italic text-center text-white bg-[#181818] ">
-            © 2024 Todos os direitos reservados por SCALA AI 
+            © 2025 Todos os direitos reservados por SCALA AI 
         </div>
     )
 }
