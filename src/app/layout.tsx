a3178c1be63140c719b84398e5768c64082e4e0d
joaootaviofm <joaootaviofm@gmail.com>
2025-04-20 23:17:16 -0300

    feat: added new components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const montserrat = Montserrat({
+const inter = Inter({
   subsets: ['latin']
 })
 
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className}`}>
+      <body className={`${inter.className} bg-[#181818]`}>
         <Navbar/>
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 // import Image from "next/image";
 
+import About from "@/components/About";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <div>
-      
+      <Hero />
+      <About/>
     </div>
   );
 }
